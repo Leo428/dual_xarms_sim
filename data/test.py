86deef1936dc7a1b3beb54479bff2156102572d1
Zheyuan Hu <huzheyuan428@gmail.com>
2025-01-20 09:30:55 -0500

    minor test mod

--- data/test.py
+++ data/test.py
@@ -3,14 +3,14 @@ import imageio
 import cv2
 
 # data = np.load("obses.npy", allow_pickle=True)
-# data = np.load("/home/huzheyuan/dual_xarms/dual_xarms_sim/data/sim_cube_1101/sim_dual_xarms_cube_handover_20241101_162055.npz", allow_pickle=True)
+data = np.load("/home/huzheyuan/dual_xarms/dual_xarms_sim/data/sim_cube_1101/sim_dual_xarms_cube_handover_20241101_162055.npz", allow_pickle=True)
 # data = np.load("/home/huzheyuan/dual_xarms/dual_xarms_sim/data/real_hang_1126/real_dual_xarms_hang_20241126_232933.npz", allow_pickle=True)
-data = np.load("/home/huzheyuan/dual_xarms/dual_xarms_sim/data/real_hang_1212/real_dual_xarms_hang_20241212_163620.npz", allow_pickle=True)
+# data = np.load("/home/huzheyuan/dual_xarms/dual_xarms_sim/data/real_hang_1212/real_dual_xarms_hang_20241212_163620.npz", allow_pickle=True)
 
 
 frames = []
 for d in data["obses"]:
-    # import ipdb; ipdb.set_trace()
+    import ipdb; ipdb.set_trace()
     images = d["images"]
     decompressed_images = {}
     for k, v in images.items():
